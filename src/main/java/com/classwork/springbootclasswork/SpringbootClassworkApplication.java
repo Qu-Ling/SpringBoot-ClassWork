e2375fa3c684fba54c15f02703b211511bc827ce
quling <x2995924975@163.com>
2024-12-16 15:21:25 +0800

    初始化项目

---
+++ src/main/java/com/classwork/springbootclasswork/SpringbootClassworkApplication.java
@@ -0,0 +1,13 @@
+package com.classwork.springbootclasswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootClassworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootClassworkApplication.class, args);
+    }
+
+}
