a44b8c9080af375b6686ec0825eaf8c0b6eb218e
quling <x2995924975@163.com>
2024-12-16 15:21:25 +0800

    初始化项目

---
+++ src/test/java/com/classwork/springbootclasswork/SpringbootClassworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classwork.springbootclasswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootClassworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
